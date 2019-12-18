a7dcdedf47e76e2fda7ea79ca7baa28fcd1fa4a5
Farexy <farexy@mail.ru>
2019-12-18 13:59:20 +0200

    remove redundant

---
+++ Assets/Scripts/MouseLook1.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseLook1 : MonoBehaviour {
+
+    public enum RotationAxes
+    {
+        MouseXandY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+
+    public RotationAxes axes = RotationAxes.MouseXandY;
+
+    public float sensitivityHor = 9.0f;
+    public float sensitivityVert = 9.0f;
+
+    public float minimumVert = -45.0f;
+    public float maximumVert = 45.0f;
+
+    private float _rotationX = 0;
+	
+	void Start () {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null)
+            body.freezeRotation = true;
+        
+	}
+	
+	
+	void Update () {
+        if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityHor, 0);
+        }
+        else if (axes == RotationAxes.MouseY)
+        {
+            //transform.Rotate(0, Input.GetAxis("Mouse Y") * sensitivityVert, 0);
+            _rotationX -= Input.GetAxis("Mouse Y")*sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            float rotationY = transform.localEulerAngles.y;
+            
+         transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+            //transform.eulerAngles = new Vector3(0, 0,_rotationX);
+        }
+        else
+        {
+            _rotationX -= Input.GetAxis("Mouse Y") * sensitivityVert;
+            _rotationX = Mathf.Clamp(_rotationX, minimumVert, maximumVert);
+
+            float delta = Input.GetAxis("Mouse X") * sensitivityHor;
+            float rotationY = transform.localEulerAngles.y + delta;
+
+            transform.localEulerAngles = new Vector3(_rotationX, rotationY, 0);
+        }
+	
+	}
+    
+}
+
+
+
+// MouseLook rotates the transform based on the mouse delta.
+// To make an FPS style character:
+// - Create a capsule.
+// - Add the MouseLook script to the capsule.
+// -> Set the mouse look to use MouseX. (You want to only turn character but not tilt it)
+// - Add FPSInput script to the capsule
+// -> A CharacterController component will be automatically added.
+//
+// - Create a camera. Make the camera a child of the capsule. Position in the head and reset the rotation.
+// - Add a MouseLook script to the camera.
+// -> Set the mouse look to use MouseY. (You want the camera to tilt up and down like a head. The character already turns.)
+
